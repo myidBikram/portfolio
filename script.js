ece245d2225da3a618a365eac358fe3c053888fa
Bikram <imbikram.jena@gmail.com>
2025-05-02 13:27:19 +0530

    page 4 and footer done and counter for pages are added

--- script.js
+++ script.js
@@ -1,75 +1,69 @@
-function loadingAnimation(){
-    let tl = gsap.timeline();
-tl.from(".line h1, .line h2, #loader_bottom h3", {
+function loadingAnimation() {
+  let tl = gsap.timeline();
+  tl.from(".line h1, .line h2, #loader_bottom h3", {
     y: 150,
     stagger: 0.25,
     duration: 0.6,
     delay: 0.5,
-});
-tl.from("#line1_counter", {
+  });
+  tl.from("#line1_counter", {
     opacity: 0,
     onStart: function () {
-        let page1Timer = document.querySelector("#line1_counter h5");
-        let count = 0;
-        let timer = setInterval(() =>{
-            count++;
-            page1Timer.innerHTML = Math.min(count, 100);
-            if (Math.min(count) >= 100) {
-                clearInterval(timer);
-            }
-            
-        } , 33);
+      let page1Timer = document.querySelector("#line1_counter h5");
+      let count = 0;
+      let timer = setInterval(() => {
+        count++;
+        page1Timer.innerHTML = Math.min(count, 100);
+        if (Math.min(count) >= 100) {
+          clearInterval(timer);
+        }
+      }, 33);
+    },
+  });
 
-    }
-})
-
-tl.to(".line h2", {
+  tl.to(".line h2", {
     opacity: 1,
-})
+  });
 
-tl.to("#loader", {
+  tl.to("#loader", {
     delay: 3.5,
     duration: 0.5,
     opacity: 0,
     ease: "power2",
     display: "none",
-    
-})
+  });
 
-tl.from("#page1", {
+  tl.from("#page1", {
     // immediateRender: false,
     delay: 0.2,
     y: "100vh",
     duration: 1,
     opacity: 0,
-})
+  });
 
-tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero3 h3, #hero3 span, #hero4",{
-    y:150,
+  tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero3 h3, #hero3 span, #hero4", {
+    y: "50vh",
     stagger: 0.2,
-})
+  });
 }
 
-function cursorAnimation(){
-    document.addEventListener("mousemove", function(dets){
+function cursorAnimation() {
+  document.addEventListener("mousemove", function (dets) {
     // console.log('dets.clientX', dets);
-    gsap.to("#crsr",{
-        x: dets.x,
-        y: dets.y,
-    })
-    
-})
+    gsap.to("#crsr", {
+      x: dets.clientX,
+      y: dets.clientY,
+      duration: 0.2,
+      ease: "power2.out",
+    });
+  });
 
-Shery.makeMagnet("#nav h4" /* Element to target.*/, {
+  Shery.makeMagnet("#nav h4, #nav img" /* Element to target.*/, {
     //Parameters are optional.
     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
     duration: 1,
   });
 }
 
-
 loadingAnimation();
 cursorAnimation();
-
-
-
